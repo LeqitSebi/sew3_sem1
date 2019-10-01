aae069e448c9badd9b30fe06b041273b3bb2a9b9
slanitsch <7225@htl.rennweg.at>
2019-10-01 12:33:29 +0200

    schreiben des Testers für toString

--- slanitsch/tests/IPAddressTest.java
+++ slanitsch/tests/IPAddressTest.java
@@ -2,21 +2,27 @@ package slanitsch.tests;
 
 import org.junit.jupiter.api.Test;
 import slanitsch.ue02_JUnit.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static slanitsch.ue02_JUnit.IPAddress.createNetmask;
 
 class IPAddressTest {
 
     @Test
-    public void testCreateIpAddress(){
+    public void testCreateIpAddress() {
         IPAddress ip = new IPAddress("8.8.8.8");
 
-        assertEquals(0x08080808, ip.getIP(), "wrong address");
+        assertEquals(0x08080808, ip.getIP(), "ipaddress wrong");
     }
 
     @Test
-    public void testCreateNetmask(){
-        assertEquals(new IPAddress ("255.255.255.0"),  createNetmask(24), "wrong subnet");
+    public void testCreateNetmask() {
+        assertEquals(new IPAddress("255.255.255.0"), createNetmask(24), "subnet wrong");
     }
 
+    @Test
+    public void testToString() {
+        IPAddress ip = new IPAddress("8.8.8.8");
+        assertEquals("IPAddress [8.8.8.8]", ip.toString(), "toString wrong");
+    }
 }

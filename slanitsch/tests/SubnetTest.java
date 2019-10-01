bf443d3659e89fa31b1f2ab3d2c9d2b79c1f90f4
slanitsch <7225@htl.rennweg.at>
2019-10-01 12:59:30 +0200

    schreiben des Testers für getMask

---
+++ slanitsch/tests/SubnetTest.java
@@ -0,0 +1,19 @@
+package slanitsch.tests;
+
+import org.junit.jupiter.api.Test;
+import slanitsch.ue02_JUnit.IPAddress;
+import slanitsch.ue02_JUnit.Subnet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubnetTest {
+
+    @Test
+    public void testCreateSubnet(){
+        IPAddress ip = new IPAddress("192.168.1.0");
+        Subnet subnet = new Subnet(ip, 24);
+
+        assertEquals(new IPAddress("255.255.255.0"), subnet.getMask(), "netmask wrong");
+    }
+
+}

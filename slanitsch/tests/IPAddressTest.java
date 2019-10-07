743fb74d104d8262b96a5bcfcf030cd5884fa922
slanitsch <7225@htl.rennweg.at>
2019-10-07 18:39:26 +0200

    fertigstellen von Aufgabe 02_JUnit

--- slanitsch/tests/IPAddressTest.java
+++ slanitsch/tests/IPAddressTest.java
@@ -11,20 +11,27 @@ class IPAddressTest {
     @Test
     public void testCreateIpAddress() {
         IPAddress ip = new IPAddress("8.8.8.8");
-
         assertEquals(0x08080808, ip.getIP(), "ipaddress wrong");
+
+        IPAddress privateIP = new IPAddress("192.168.0.1");
+        assertEquals(0xc0a80001, privateIP.getIP());
     }
 
     @Test
     public void testCreateNetmask() {
         assertEquals(new IPAddress("255.255.255.0"), createNetmask(24), "subnet wrong");
+
+        IPAddress ip = new IPAddress("8.8.8.8");
+        assertEquals(0b11111111_11111111_11111111_00000000, createNetmask(24).getIP());
     }
 
     @Test
     public void testToString() {
         IPAddress ip = new IPAddress("8.8.8.8");
-
         assertEquals("IPAddress [8.8.8.8]", ip.toString(), "toString wrong");
+
+        IPAddress ip2 = new IPAddress("192.168.0.1");
+        assertEquals("IPAddress [192.168.0.1]", ip2.toString());
     }
 
     @Test
--- slanitsch/tests/SubnetTest.java
+++ slanitsch/tests/SubnetTest.java
@@ -9,26 +9,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class SubnetTest {
 
     @Test
-    public void testGetNet(){
+    public void testGetNet() {
         IPAddress ip = new IPAddress("192.168.1.0");
         Subnet subnet = new Subnet(ip, 24);
-
         assertEquals(new IPAddress("192.168.1.0"), subnet.getNet(), "network wrong");
+
+        Subnet sub2 = new Subnet(new IPAddress("10.1.0.0"), 27);
+        assertEquals(0x0a010000, sub2.getNet().getIP());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Subnet(new IPAddress("8.8.8.8"), 27).getNet().getIP();
+        });
     }
 
     @Test
-    public void testGetMask(){
+    public void testGetMask() {
         IPAddress ip = new IPAddress("192.168.1.0");
         Subnet subnet = new Subnet(ip, 24);
-
         assertEquals(new IPAddress("255.255.255.0"), subnet.getMask(), "netmask wrong");
+
+        Subnet sub = new Subnet(new IPAddress("192.168.0.0"), 28);
+        assertEquals(0b11111111_11111111_11111111_11110000, sub.getMask().getIP());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Subnet(new IPAddress("8.8.8.8"), 27);
+        });
     }
 
     @Test
-    public void testGetBroadcast(){
+    public void testGetBroadcast() {
         IPAddress ip = new IPAddress("192.168.1.0");
         Subnet subnet = new Subnet(ip, 24);
-
         assertEquals(new IPAddress("192.168.1.255"), subnet.getBroadcast(), "broadcast wrong");
+
+        Subnet sub2 = new Subnet(new IPAddress("10.1.0.0"), 27);
+        assertEquals(0x0a01001f, sub2.getBroadcast().getIP());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Subnet(new IPAddress("8.8.8.8"), 27).getBroadcast().getIP();
+        });
     }
+
+    @Test
+    public void testToString(){
+
+        Subnet sub = new Subnet(new IPAddress("192.168.0.0"),28);
+        assertEquals("Subnet [net=IPAddress [192.168.0.0], mask=IPAddress [255.255.255.240]]",sub.toString());
+
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Subnet(new IPAddress("8.8.8.8"), 27).toString();
+        });
+    }
+
 }
